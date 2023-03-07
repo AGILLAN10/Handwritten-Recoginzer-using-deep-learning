17f01f4c2e080ba71b131142b725f5c155198f9b
AGILLAN10 <112199545+AGILLAN10@users.noreply.github.com>
2023-03-07 18:54:55 +0530

    Add files via upload

---
+++ Handwritten Recognizer_basic_ANN_Model.py
@@ -0,0 +1,145 @@
+#!/usr/bin/env python
+# coding: utf-8
+
+# In[1]:
+
+
+pip install opencv-python
+
+
+# In[2]:
+
+
+pip install tensorflow
+
+
+# In[3]:
+
+
+import cv2
+import numpy as np
+import matplotlib.pyplot as plt
+import tensorflow as tf
+import os
+
+
+# In[4]:
+
+
+#loading dataset
+mnist=tf.keras.datasets.mnist
+
+
+# In[5]:
+
+
+#splitting
+(x_train,y_train),(x_test,y_test)=mnist.load_data()
+
+
+# In[6]:
+
+
+#normalization
+x_train=tf.keras.utils.normalize(x_train,axis=1)
+y_test=tf.keras.utils.normalize(x_test,axis=1)
+
+
+# In[7]:
+
+
+#creating basic model
+model=tf.keras.models.Sequential()
+model.add(tf.keras.layers.Flatten(input_shape=(28,28)))
+model.add(tf.keras.layers.Dense(200,activation='relu'))
+model.add(tf.keras.layers.Dense(200,activation='relu'))
+model.add(tf.keras.layers.Dense(10,activation='softmax'))
+
+
+# In[ ]:
+
+
+
+
+
+# In[8]:
+
+
+#compiling the model
+model.compile(optimizer='adam',loss='sparse_categorical_crossentropy',metrics=['accuracy'])
+
+
+# In[9]:
+
+
+#fitting the model
+model.fit(x_train,y_train,epochs=3)
+
+
+# In[10]:
+
+
+model.save('handwritten.model')
+
+
+# In[11]:
+
+
+model1=tf.keras.models.load_model('handwritten.model')
+
+
+# In[12]:
+
+
+model1.evaluate(x_test,y_test)
+
+
+# In[ ]:
+
+
+print(loss)
+print(accuracy)
+
+
+# In[ ]:
+
+
+model.predict(y_test)
+
+
+# In[13]:
+
+
+image_no=1
+
+while os.path.isfile(f"C:/Users/91936/Documents/Digit_recognizer/testingimg/digits{image_no}.png"):
+    try:
+        img=cv2.imread(f"C:/Users/91936/Documents/Digit_recognizer/testingimg/digits{image_no}.png")[:,:,0]
+        img=np.invert(np.array([img]))
+        pred=model.predict(img)
+        print(f"The digit is {np.argmax(pred)}")
+        plt.imshow(img[0],cmap=plt.cm.binary)
+        plt.show()
+    except:
+        print("Error")
+    finally:
+        image_no+=1
+
+
+# In[ ]:
+
+
+
+
+
+# In[ ]:
+
+
+
+
+
+# In[ ]:
+
+
+
+
